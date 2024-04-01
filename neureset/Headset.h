98eb06d1caf3f4bf5c6fc6e40fd6ef47fe0581cc
Briana <lee.briana@gmail.com>
2024-04-01 16:10:08 -0400

    Added core Headset class functions

---
+++ neureset/Headset.h
@@ -0,0 +1,22 @@
+#ifndef HEADSET_H
+#define HEADSET_H
+
+#include <QObject>
+#include <QVector>
+
+
+class Headset : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Headset(QObject *parent = nullptr);
+
+    void applyTreatment(int site, int baseline);
+    int readEEGBaseline(int site);
+    QVector<int> readBaseline();
+
+public slots:
+    void readBaselineSlot();
+
+};
+#endif // HEADSET_H
